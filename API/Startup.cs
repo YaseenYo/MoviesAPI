723047c5a8a4ed9eb31d70a411bd4f9ee8a7f8f5
mohammed17yo <mohammed17yo@gmail.com>
2022-01-26 11:37:01 +0530

    using statement changes

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +15,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using MoviesRentalAPI.DbContexts;
 using MoviesRentalAPI.Services;
+using Newtonsoft.Json.Serialization;
 
 namespace API
 {
